ad2fa86db8ea7e82edca1e4dae43c3a7e5c91ad7
DragonHeart69 <dimitri@fam-decrock.eu>
2022-11-04 14:33:07 +0100

    start with error

---
+++ README.md
@@ -0,0 +1,5 @@
+# Snapchat Recipes
+
+This one won't work for now. If anyone can help me with this, feel free to PR me.
+
+##
---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports = Ferdi => class SnapChat extends Ferdi{
+    overrideUserAgent() {
+        return "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36";
+      }
+};
